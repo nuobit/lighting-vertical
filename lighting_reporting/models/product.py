f3d4f94cf600fadd32f9db685a61adbf6e2fad99
Eric Antones <eantones@nuobit.com>
2018-10-10 13:24:38 +0200

    [MIG] lighting_reporting: migrate from 11.0

---
+++ lighting_reporting/models/product.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class LightingProduct(models.Model):
+    _inherit = 'lighting.product'
+
+    @api.multi
+    def print_product(self):
+        return self.env.ref('lighting_reporting.action_report_product').report_action(self)
+
+class LightingAttachment(models.Model):
+    _inherit = 'lighting.attachment'
+
+    show_in_data_sheet = fields.Boolean(string='Show in data sheet')
