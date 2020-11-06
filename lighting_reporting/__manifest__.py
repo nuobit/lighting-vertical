1ca13acd94a1aef11ffcf24fe1e5a3f810af279b
Eric Antones <eantones@nuobit.com>
2020-11-06 12:24:18 +0100

    [IMP] lighting_reporting: show total wattage if product is an accessory

--- lighting_reporting/__manifest__.py
+++ lighting_reporting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Reporting",
     'description': """* Product datasheet""",
-    'version': '11.0.0.13.9',
+    'version': '11.0.0.13.10',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
