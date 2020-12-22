e3d7d0669bcedef0dfdf0fe22862c4a9ba40332b
Eric Antones <eantones@nuobit.com>
2020-12-22 23:50:50 +0100

    [REF] lighting: refactor method name

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.92.13',
+    'version': '11.0.0.92.14',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
