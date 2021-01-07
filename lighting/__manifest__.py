6941c4b8bb8c4864b5b2ffc79319bd52feeabf52
Eric Antones <eantones@nuobit.com>
2021-01-07 14:45:09 +0100

    [IMP] lighting: added historical end of stock

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.92.15',
+    'version': '11.0.0.92.16',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
