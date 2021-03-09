c47ff5881d6a5d51b0ca69c176f334cc33c74344
Eric Antones <eantones@nuobit.com>
2021-03-09 23:41:23 +0100

    [ADD] lighting_attachment_package: packing attachments

---
+++ lighting_attachment_package/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Lighting Attachment Package",
+    'description': "Generate attachment packages",
+    'version': '11.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.nuobit.com',
+    'depends': [
+        'lighting',
+        'lighting_seo',
+        'lighting_reporting',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_attachment_views.xml',
+    ],
+    'installable': True,
+}
