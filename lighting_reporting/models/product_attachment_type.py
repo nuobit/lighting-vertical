6a92d51c64e8af677c3eae427dde4481a758e746
Eric Antones <eantones@nuobit.com>
2022-03-28 19:47:21 +0200

    [IMP] lighting_reporting: Allows to include pdf attachments into Datasheets

---
+++ lighting_reporting/models/product_attachment_type.py
@@ -0,0 +1,11 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+
+
+class LightingAttachmentType(models.Model):
+    _inherit = 'lighting.attachment.type'
+
+    include_in_datasheet = fields.Boolean(string='Include in Datasheet')
