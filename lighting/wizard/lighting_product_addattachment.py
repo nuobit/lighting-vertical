2b928f2a494dd12c4b9f6d6e46147d106eaf9826
Eric Antones <eantones@nuobit.com>
2022-04-21 20:11:15 +0200

    [IMP] lighting: Allow overwriting attachments on attach wizard

--- lighting/wizard/lighting_product_addattachment.py
+++ lighting/wizard/lighting_product_addattachment.py
@@ -32,6 +32,8 @@ class LightingProductAddAttachment(models.TransientModel):
 
     lang_id = fields.Many2one(comodel_name='lighting.language', ondelete='cascade', string='Language')
 
+    overwrite = fields.Boolean(string='Overwrite', default=False)
+
     result = fields.Char(string='Result', readonly=True)
 
     state = fields.Selection([
@@ -74,11 +76,14 @@ class LightingProductAddAttachment(models.TransientModel):
 
         # construct the values
         values = {
-            'name': self.name,
             'type_id': self.type_id.id,
-            'lang_id': self.lang_id.id,
             'datas_location': self.datas_location,
         }
+        if self.name:
+            values['name'] = self.name
+        if self.lang_id:
+            values['lang_id'] = self.lang_id.id
+
         if self.datas_location == 'file':
             values.update({
                 'datas': self.datas,
@@ -97,11 +102,9 @@ class LightingProductAddAttachment(models.TransientModel):
         errors = {}
         for product in products:
             # check if attach already exists
+            attach_grouped = {}
             for attach in product.attachment_ids.filtered(
-                    lambda x: x.datas_location == self.datas_location):
-                # check if already exists another attachment linked
-                # with the same file or url
-                duplicated = False
+                    lambda x: x.datas_location == self.datas_location and x.type_id == self.type_id):
                 if attach.datas_location == 'file':
                     # get product Odoo attach object
                     ir_attach = self.env['ir.attachment'].search(
@@ -112,30 +115,39 @@ class LightingProductAddAttachment(models.TransientModel):
                     if not ir_attach:
                         continue
                     ir_attach = ir_attach[0]
-                    duplicated = ir_attach.checksum == addattach.checksum
-                elif attach.datas_location == 'url':
-                    duplicated = ir_attach.datas_url == addattach.datas_url
-
-                if duplicated:
-                    errors.setdefault(product.id, {'product': product})
-                    if attach.type_id == self.type_id:
-                        errors[product.id]['duplicated_same_type'] = True
-                        break
+                    if ir_attach.checksum == addattach.checksum:
+                        attach_grouped.setdefault('bydatas', []).append(attach)
                     else:
-                        errors[product.id].setdefault('duplicated_diff_type', []) \
-                            .append(attach.type_id)
+                        if attach.datas_fname.lower() == self.datas_fname.lower():
+                            attach_grouped.setdefault('byfname', []).append(attach)
+                elif attach.datas_location == 'url':
+                    if ir_attach.datas_url.lower() == addattach.datas_url.lower():
+                        attach_grouped.setdefault('bydatas', []).append(attach)
 
-            if product.id not in errors:
+            if attach_grouped:
+                for gtype, attachs in attach_grouped.items():
+                    if gtype == 'bydatas':
+                        errors.setdefault(product.id, {'product': product})['same_checksum_match'] = attachs
+                    elif gtype == 'byfname':
+                        if len(attachs) > 1:
+                            errors.setdefault(product.id, {'product': product})['more_than_one_fname_match'] = attachs
+                        elif len(attachs) == 1:
+                            if self.overwrite:
+                                product.attachment_ids = [(1, attachs[0].id, values)]
+                            else:
+                                product.attachment_ids = [(0, False, values)]
+            else:
                 product.attachment_ids = [(0, False, values)]
 
         msg = []
         for data in errors.values():
             msg0 = []
-            if 'duplicated_same_type' in data:
-                msg0.append(_('Already exists an attachment with the same type'))
-            if 'duplicated_diff_type' in data:
-                msg0.append(_('Already exists an attachment but with different types: %s') %
-                            ', '.join({x.display_name for x in data['duplicated_diff_type']}))
+            if 'same_checksum_match' in data:
+                msg0.append(_('Already exists other attachment with exactly the same content: %s') %
+                            ', '.join({x.display_name for x in data['same_checksum_match']}))
+            if 'more_than_one_fname_match' in data:
+                msg0.append(_('More than one attachment found with the same name: %s') %
+                            ', '.join({x.display_name for x in data['more_than_one_fname_match']}))
             msg.append('> %s: %s' % (data['product'].reference, ', '.join(msg0)))
 
         if msg != []:
