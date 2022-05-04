6309b640a8177a6da99f0988acf300c83fe5344e
KNVx <kniubo@nuobit.com>
2022-05-04 16:47:08 +0200

    [ADD] connector_lighting_sapb1: this new module it's made to rename the old connector_sapb1 (lighting)

---
+++ connector_lighting_sapb1/__manifest__.py
@@ -0,0 +1,34 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Connector Lighting SAP B1",
+    'description': "SAP Business One connector",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Connector',
+    'website': 'https://github.com/nuobit',
+    'external_dependencies': {
+        'python': [
+            'paramiko',
+            'hdbcli',
+            'requests',
+        ],
+    },
+    'depends': [
+        'connector',
+        'lighting',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/connector_sapb1.xml',
+        'data/ir_cron.xml',
+        'views/sapb1_backend_view.xml',
+        'views/lighting_product_view.xml',
+        'views/connector_sapb1_menu.xml',
+    ],
+    'installable': True,
+    'post_init_hook': 'post_init_hook',
+}
---
+++ connector_lighting_sapb1/models/lighting_product/importer.py
@@ -0,0 +1,57 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo.addons.component.core import Component
+
+
+class LightingProductImporter(Component):
+    _name = 'sapb1.lighting.product.importer'
+    _inherit = 'sapb1.lighting.importer'
+
+    _apply_on = 'sapb1.lighting.product'
+
+    def _find_existing(self, external_id):
+        """ Find existing record by external_id  """
+        adapter = self.component(usage='backend.adapter', model_name=self.model)
+        external_id_d = adapter.id2dict(external_id)
+
+        reference = external_id_d['ItemCode']
+        if reference:
+            product = self.env['lighting.product'].search([
+                ('reference', '=', reference),
+            ])
+            if product:
+                if len(product) > 1:
+                    raise Exception("There's more than one existing product "
+                                    "with the same Reference %s" % reference)
+                return {
+                    'odoo_id': product.id,
+                }
+
+        return None
+
+    def _mapper_options(self, binding):
+        return {'binding': binding}
+
+
+class LightingProductDirectBatchImporter(Component):
+    """ Import the SAP B1 Lighting Products.
+
+    For every product in the list, import it directly.
+    """
+    _name = 'sapb1.lighting.product.direct.batch.importer'
+    _inherit = 'sapb1.lighting.direct.batch.importer'
+
+    _apply_on = 'sapb1.lighting.product'
+
+
+class LightingProductDelayedBatchImporter(Component):
+    """ Import the SAP B1 Lighting Products.
+
+    For every product in the list, a delayed job is created.
+    """
+    _name = 'sapb1.lighting.product.delayed.batch.importer'
+    _inherit = 'sapb1.lighting.delayed.batch.importer'
+
+    _apply_on = 'sapb1.lighting.product'
