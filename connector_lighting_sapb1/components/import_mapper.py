86782cbe59a3f3447f49092a3788f632c3ded08b
KNVx <kniubo@nuobit.com>
2022-05-04 16:47:08 +0200

    [ADD] connector_lighting_sapb1: this new module it's made to rename the old connector_sapb1 (lighting)

---
+++ connector_lighting_sapb1/components/import_mapper.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo.addons.component.core import AbstractComponent
+
+
+class SAPB1LightingLightingImportMapper(AbstractComponent):
+    _name = 'sapb1.lighting.import.mapper'
+    _inherit = ['base.import.mapper', 'base.sapb1.lighting.connector']
+
+
+class SAPB1LightingImportMapChild(AbstractComponent):
+    _name = 'sapb1.lighting.map.child.import'
+    _inherit = ['base.map.child.import', 'base.sapb1.lighting.connector']
