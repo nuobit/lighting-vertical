ffd4750a7ee9bc5b91433642e845551067ea2f81
Eric Antones <eantones@nuobit.com>
2022-10-24 16:27:52 +0200

    [IMP] lighting: Added new "model" field to products

---
+++ lighting/models/product_model.py
@@ -0,0 +1,24 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class LightingProductModel(models.Model):
+    _name = 'lighting.product.model'
+
+    name = fields.Char(string='Name', required=True)
+
+    product_count = fields.Integer(compute='_compute_product_count', string='Product(s)')
+
+    def _compute_product_count(self):
+        for record in self:
+            record.product_count = self.env['lighting.product'].search_count([
+                ('model_id', '=', record.id)
+            ])
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'The model name must be unique!'),
+    ]
