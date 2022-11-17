c50cc64c99d6338cf23d62bb8dbbf53ec9dec6c5
Eric Antones <eantones@nuobit.com>
2022-11-17 14:34:32 +0100

    [FIX] lighting: added empty string wattage instead if being ignored if null

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.99.15',
+    'version': '11.0.0.99.16',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
