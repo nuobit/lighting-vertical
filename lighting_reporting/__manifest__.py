933d95b778e2425eab4b845b2624028ffa892c1f
KNVx <kniubo@nuobit.com>
2022-12-09 16:18:38 +0100

    [IMP] lighting_reporting: new field efficiency_id on report

--- lighting_reporting/__manifest__.py
+++ lighting_reporting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Reporting",
     'description': """* Product datasheet""",
-    'version': '11.0.0.15.5',
+    'version': '11.0.0.15.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
