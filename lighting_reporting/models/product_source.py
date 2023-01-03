25fa108e2b54f6256c1c18db994bc00b75cfc6a8
KNVx <kniubo@nuobit.com>
2023-01-03 16:58:24 +0100

    [IMP] lighting_reporting: refactor function to print efficiency from source when cct don't have information

--- lighting_reporting/models/product_source.py
+++ lighting_reporting/models/product_source.py
@@ -13,15 +13,22 @@ class LightingProductSource(models.Model):
         ccts_by_ef = {}
         cct_flux = self.mapped('line_ids.color_temperature_flux_ids').sorted(
             lambda x: (x.efficiency_id.sequence, x.color_temperature_id.value))
-        for line in cct_flux:
-            if line.efficiency_id:
-                ccts_by_ef.setdefault(line.efficiency_id, self.env[line.color_temperature_id._name])
-                ccts_by_ef[line.efficiency_id] |= line.color_temperature_id
+        if cct_flux.mapped('efficiency_id'):
+            for line in cct_flux:
+                if line.efficiency_id:
+                    ccts_by_ef.setdefault(line.efficiency_id, self.env[line.color_temperature_id._name])
+                    ccts_by_ef[line.efficiency_id] |= line.color_temperature_id
+        else:
+            for efficiency in self.line_ids.efficiency_ids:
+                ccts_by_ef[efficiency] = cct_flux.mapped('color_temperature_id')
         return ccts_by_ef
 
     @api.model
     def energy_efficiency_display(self, ccts_by_ef):
         efs = []
         for efficiency, ccts in ccts_by_ef.items():
-            efs.append("%s (%s)" % (efficiency.name, ', '.join([x.display_name for x in ccts])))
+            efs_l = [efficiency.name]
+            if ccts:
+                efs_l.append("(%s)" % ', '.join([x.display_name for x in ccts]))
+            efs.append(" ".join(efs_l))
         return ', '.join(efs)
