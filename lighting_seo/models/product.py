2acebad94ca6b9419700089fc0eae2e6b55f9844
KNVx <kniubo@nuobit.com>
2023-12-04 10:15:47 +0100

    [IMP] lighting_seo: pre-commit stuff

--- lighting_seo/models/product.py
+++ lighting_seo/models/product.py
@@ -2,10 +2,10 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.lighting.models.product import ES_MAP, D_MAP, C_STATES
-
-from odoo import fields, models, api, _
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+
+from odoo.addons.lighting.models.product import C_STATES, D_MAP, ES_MAP
 
 MIN_STOCK = 10
 
@@ -20,7 +20,9 @@ def seo_preview(title, url, description):
     """
     template_title = '<li style="margin-bottom:0;color:#1a0dab;font-size:18px;">%s</li>'
     template_url = '<li style="margin-bottom:0;color:#006621;font-size:14px;">%s</li>'
-    template_description = '<li style="margin-bottom:0;color:#545454;font-size:small;">%s</li>'
+    template_description = (
+        '<li style="margin-bottom:0;color:#545454;font-size:small;">%s</li>'
+    )
 
     preview = []
     if title:
@@ -32,40 +34,53 @@ def seo_preview(title, url, description):
 
     meta_preview = False
     if preview:
-        meta_preview = template % ''.join(preview)
+        meta_preview = template % "".join(preview)
 
     return meta_preview
 
 
 class LightingProduct(models.Model):
-    _inherit = 'lighting.product'
+    _inherit = "lighting.product"
 
-    website_published = fields.Boolean('Published on Website', copy=False, track_visibility='onchange')
+    website_published = fields.Boolean(
+        "Published on Website", copy=False, track_visibility="onchange"
+    )
 
-    website_published_readonly = fields.Boolean(compute="_compute_website_published_readonly")
+    website_published_readonly = fields.Boolean(
+        compute="_compute_website_published_readonly"
+    )
 
     @api.multi
     def _compute_website_published_readonly(self):
         for rec in self:
-            rec.website_published_readonly = not rec.user_has_groups('lighting_seo.group_lighting_ecommerce_manager')
+            rec.website_published_readonly = not rec.user_has_groups(
+                "lighting_seo.group_lighting_ecommerce_manager"
+            )
 
     @api.multi
     def website_publish_button(self):
         self.ensure_one()
-        return self.write({'website_published': not self.website_published})
+        return self.write({"website_published": not self.website_published})
 
-    seo_title = fields.Char(string='Meta title', translate=True)
-    seo_url = fields.Char(string='URL')
-    seo_description = fields.Text(string='Meta description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
-                                       relation='lighting_product_seo_keyword_rel',
-                                       column1='product_id', column2='keyword_id', string='Keywords')
+    seo_title = fields.Char(string="Meta title", translate=True)
+    seo_url = fields.Char(string="URL")
+    seo_description = fields.Text(string="Meta description", translate=True)
+    seo_keyword_ids = fields.Many2many(
+        comodel_name="lighting.seo.keyword",
+        relation="lighting_product_seo_keyword_rel",
+        column1="product_id",
+        column2="keyword_id",
+        string="Keywords",
+    )
 
-    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
-                                             readonly=True)
+    meta_title_length = fields.Integer(
+        string="Meta title length", compute="_compute_lengths", readonly=True
+    )
+    meta_description_length = fields.Integer(
+        string="Meta description length", compute="_compute_lengths", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_description')
+    @api.depends("seo_title", "seo_description")
     def _compute_lengths(self):
         for rec in self:
             if rec.seo_title:
@@ -73,45 +88,64 @@ class LightingProduct(models.Model):
             if rec.seo_description:
                 rec.meta_description_length = len(rec.seo_description)
 
-    meta_preview = fields.Char(string='Preview', compute='_compute_preview', readonly=True)
+    meta_preview = fields.Char(
+        string="Preview", compute="_compute_preview", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_url', 'seo_description')
+    @api.depends("seo_title", "seo_url", "seo_description")
     def _compute_preview(self):
         for rec in self:
-            rec.meta_preview = seo_preview(rec.seo_title, rec.seo_url, rec.seo_description)
+            rec.meta_preview = seo_preview(
+                rec.seo_title, rec.seo_url, rec.seo_description
+            )
 
-    marketplace_title = fields.Char(string='Marketplace Title', translate=True, track_visibility='onchange')
-    marketplace_description = fields.Text(string='Marketplace Description', translate=True, track_visibility='onchange')
+    marketplace_title = fields.Char(
+        string="Marketplace Title", translate=True, track_visibility="onchange"
+    )
+    marketplace_description = fields.Text(
+        string="Marketplace Description", translate=True, track_visibility="onchange"
+    )
 
     def _check_state_marketing_stock(self, values):
         new_values = super(LightingProduct, self)._check_state_marketing_stock(values)
         values.update(new_values)
-        current_state, new_state = self.state_marketing, values.get('state_marketing', self.state_marketing)
+        current_state, new_state = self.state_marketing, values.get(
+            "state_marketing", self.state_marketing
+        )
         current_stock = self.available_qty + self.stock_future_qty
-        new_stock = sum([values[f] if f in values else self[f] for f in ('available_qty', 'stock_future_qty')])
+        new_stock = sum(
+            [
+                values[f] if f in values else self[f]
+                for f in ("available_qty", "stock_future_qty")
+            ]
+        )
         if current_state in C_STATES:
             if new_state in D_MAP:
-                self._update_with_check(new_values, 'website_published', False)
+                self._update_with_check(new_values, "website_published", False)
         elif current_state in ES_MAP:
             if new_state == current_state:
                 if new_stock > current_stock:
                     if new_stock >= MIN_STOCK:
-                        self._update_with_check(new_values, 'website_published', True)
+                        self._update_with_check(new_values, "website_published", True)
             elif new_state == ES_MAP[current_state]:
-                self._update_with_check(new_values, 'website_published', False)
+                self._update_with_check(new_values, "website_published", False)
         elif current_state in D_MAP:
             if new_state == current_state:
-                self._update_with_check(new_values, 'website_published', False)
+                self._update_with_check(new_values, "website_published", False)
             elif new_state == D_MAP[current_state]:
-                self._update_with_check(new_values, 'website_published', new_stock >= MIN_STOCK)
+                self._update_with_check(
+                    new_values, "website_published", new_stock >= MIN_STOCK
+                )
         return new_values
 
     @api.multi
     def _check_website_published_readonly(self, vals):
-        if 'website_published' in vals:
+        if "website_published" in vals:
             for rec in self:
                 if rec.website_published_readonly:
-                    raise ValidationError("You have no permissions to modify the Website Published field")
+                    raise ValidationError(
+                        "You have no permissions to modify the Website Published field"
+                    )
 
     @api.multi
     def write(self, vals):
@@ -125,20 +159,27 @@ class LightingProduct(models.Model):
 
 
 class LightingProductFamily(models.Model):
-    _inherit = 'lighting.product.family'
+    _inherit = "lighting.product.family"
 
-    seo_title = fields.Char(string='Meta title', translate=True)
-    seo_url = fields.Char(string='URL')
-    seo_description = fields.Text(string='Meta description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
-                                       relation='lighting_product_family_seo_keyword_rel',
-                                       column1='family_id', column2='keyword_id', string='Keywords')
+    seo_title = fields.Char(string="Meta title", translate=True)
+    seo_url = fields.Char(string="URL")
+    seo_description = fields.Text(string="Meta description", translate=True)
+    seo_keyword_ids = fields.Many2many(
+        comodel_name="lighting.seo.keyword",
+        relation="lighting_product_family_seo_keyword_rel",
+        column1="family_id",
+        column2="keyword_id",
+        string="Keywords",
+    )
 
-    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
-                                             readonly=True)
+    meta_title_length = fields.Integer(
+        string="Meta title length", compute="_compute_lengths", readonly=True
+    )
+    meta_description_length = fields.Integer(
+        string="Meta description length", compute="_compute_lengths", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_description')
+    @api.depends("seo_title", "seo_description")
     def _compute_lengths(self):
         for rec in self:
             if rec.seo_title:
@@ -146,29 +187,40 @@ class LightingProductFamily(models.Model):
             if rec.seo_description:
                 rec.meta_description_length = len(rec.seo_description)
 
-    meta_preview = fields.Char(string='Preview', compute='_compute_preview', readonly=True)
+    meta_preview = fields.Char(
+        string="Preview", compute="_compute_preview", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_url', 'seo_description')
+    @api.depends("seo_title", "seo_url", "seo_description")
     def _compute_preview(self):
         for rec in self:
-            rec.meta_preview = seo_preview(rec.seo_title, rec.seo_url, rec.seo_description)
+            rec.meta_preview = seo_preview(
+                rec.seo_title, rec.seo_url, rec.seo_description
+            )
 
 
 class LightingProductCategory(models.Model):
-    _inherit = 'lighting.product.category'
+    _inherit = "lighting.product.category"
 
-    seo_title = fields.Char(string='Meta title', translate=True)
-    seo_url = fields.Char(string='URL')
-    seo_description = fields.Char(string='Meta description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
-                                       relation='lighting_product_category_seo_keyword_rel',
-                                       column1='category_id', column2='keyword_id', string='Keywords')
+    seo_title = fields.Char(string="Meta title", translate=True)
+    seo_url = fields.Char(string="URL")
+    seo_description = fields.Char(string="Meta description", translate=True)
+    seo_keyword_ids = fields.Many2many(
+        comodel_name="lighting.seo.keyword",
+        relation="lighting_product_category_seo_keyword_rel",
+        column1="category_id",
+        column2="keyword_id",
+        string="Keywords",
+    )
 
-    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
-                                             readonly=True)
+    meta_title_length = fields.Integer(
+        string="Meta title length", compute="_compute_lengths", readonly=True
+    )
+    meta_description_length = fields.Integer(
+        string="Meta description length", compute="_compute_lengths", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_description')
+    @api.depends("seo_title", "seo_description")
     def _compute_lengths(self):
         for rec in self:
             if rec.seo_title:
@@ -176,29 +228,40 @@ class LightingProductCategory(models.Model):
             if rec.seo_description:
                 rec.meta_description_length = len(rec.seo_description)
 
-    meta_preview = fields.Char(string='Preview', compute='_compute_preview', readonly=True)
+    meta_preview = fields.Char(
+        string="Preview", compute="_compute_preview", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_url', 'seo_description')
+    @api.depends("seo_title", "seo_url", "seo_description")
     def _compute_preview(self):
         for rec in self:
-            rec.meta_preview = seo_preview(rec.seo_title, rec.seo_url, rec.seo_description)
+            rec.meta_preview = seo_preview(
+                rec.seo_title, rec.seo_url, rec.seo_description
+            )
 
 
 class LightingProductApplication(models.Model):
-    _inherit = 'lighting.product.application'
+    _inherit = "lighting.product.application"
 
-    seo_title = fields.Char(string='Meta title', translate=True)
-    seo_url = fields.Char(string='URL')
-    seo_description = fields.Char(string='Meta description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
-                                       relation='lighting_product_application_seo_keyword_rel',
-                                       column1='application_id', column2='keyword_id', string='Keywords')
+    seo_title = fields.Char(string="Meta title", translate=True)
+    seo_url = fields.Char(string="URL")
+    seo_description = fields.Char(string="Meta description", translate=True)
+    seo_keyword_ids = fields.Many2many(
+        comodel_name="lighting.seo.keyword",
+        relation="lighting_product_application_seo_keyword_rel",
+        column1="application_id",
+        column2="keyword_id",
+        string="Keywords",
+    )
 
-    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
-                                             readonly=True)
+    meta_title_length = fields.Integer(
+        string="Meta title length", compute="_compute_lengths", readonly=True
+    )
+    meta_description_length = fields.Integer(
+        string="Meta description length", compute="_compute_lengths", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_description')
+    @api.depends("seo_title", "seo_description")
     def _compute_lengths(self):
         for rec in self:
             if rec.seo_title:
@@ -206,28 +269,39 @@ class LightingProductApplication(models.Model):
             if rec.seo_description:
                 rec.meta_description_length = len(rec.seo_description)
 
-    meta_preview = fields.Char(string='Preview', compute='_compute_preview', readonly=True)
+    meta_preview = fields.Char(
+        string="Preview", compute="_compute_preview", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_url', 'seo_description')
+    @api.depends("seo_title", "seo_url", "seo_description")
     def _compute_preview(self):
         for rec in self:
-            rec.meta_preview = seo_preview(rec.seo_title, rec.seo_url, rec.seo_description)
+            rec.meta_preview = seo_preview(
+                rec.seo_title, rec.seo_url, rec.seo_description
+            )
 
 
 class LightingCatalog(models.Model):
-    _inherit = 'lighting.catalog'
+    _inherit = "lighting.catalog"
 
-    seo_title = fields.Char(string='Meta title', translate=True)
-    seo_url = fields.Char(string='URL')
-    seo_description = fields.Char(string='Meta description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
-                                       relation='lighting_catalog_seo_keyword_rel',
-                                       column1='catalog_id', column2='keyword_id', string='Keywords')
-    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
-                                             readonly=True)
+    seo_title = fields.Char(string="Meta title", translate=True)
+    seo_url = fields.Char(string="URL")
+    seo_description = fields.Char(string="Meta description", translate=True)
+    seo_keyword_ids = fields.Many2many(
+        comodel_name="lighting.seo.keyword",
+        relation="lighting_catalog_seo_keyword_rel",
+        column1="catalog_id",
+        column2="keyword_id",
+        string="Keywords",
+    )
+    meta_title_length = fields.Integer(
+        string="Meta title length", compute="_compute_lengths", readonly=True
+    )
+    meta_description_length = fields.Integer(
+        string="Meta description length", compute="_compute_lengths", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_description')
+    @api.depends("seo_title", "seo_description")
     def _compute_lengths(self):
         for rec in self:
             if rec.seo_title:
@@ -235,9 +309,13 @@ class LightingCatalog(models.Model):
             if rec.seo_description:
                 rec.meta_description_length = len(rec.seo_description)
 
-    meta_preview = fields.Char(string='Preview', compute='_compute_preview', readonly=True)
+    meta_preview = fields.Char(
+        string="Preview", compute="_compute_preview", readonly=True
+    )
 
-    @api.depends('seo_title', 'seo_url', 'seo_description')
+    @api.depends("seo_title", "seo_url", "seo_description")
     def _compute_preview(self):
         for rec in self:
-            rec.meta_preview = seo_preview(rec.seo_title, rec.seo_url, rec.seo_description)
+            rec.meta_preview = seo_preview(
+                rec.seo_title, rec.seo_url, rec.seo_description
+            )
