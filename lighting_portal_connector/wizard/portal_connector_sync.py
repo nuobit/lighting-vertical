c7056f8ddd96a853a9b37850ef58836a23e195ed
KNVx <kniubo@nuobit.com>
2023-12-05 16:18:02 +0100

    [IMP] lighting_portal_connector: pre-commit stuff

--- lighting_portal_connector/wizard/portal_connector_sync.py
+++ lighting_portal_connector/wizard/portal_connector_sync.py
@@ -2,9 +2,10 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
 import logging
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -14,19 +15,28 @@ class LightingPortalConnectorSync(models.TransientModel):
 
     @api.model
     def synchronize(self, ids=None, context=None, reference=None):
-        if not reference: _logger.info('Start syncronization')
-        settings = self.env['lighting.portal.connector.settings'].search([]).sorted(lambda x: x.sequence)
+        if not reference:
+            _logger.info("Start syncronization")
+        settings = (
+            self.env["lighting.portal.connector.settings"]
+            .search([])
+            .sorted(lambda x: x.sequence)
+        )
         if settings:
             settings = settings[0]
         else:
-            raise UserError(_("No configuration present, please configure database server"))
+            raise UserError(
+                _("No configuration present, please configure database server")
+            )
 
         from hdbcli import dbapi
 
-        conn = dbapi.connect(settings['host'],
-                             settings['port'],
-                             settings['username'],
-                             settings['password'])
+        conn = dbapi.connect(
+            settings["host"],
+            settings["port"],
+            settings["username"],
+            settings["password"],
+        )
 
         cursor = conn.cursor()
 
@@ -34,13 +44,17 @@ class LightingPortalConnectorSync(models.TransientModel):
         stmnt = "SELECT SCHEMA_NAME FROM SCHEMAS"
         cursor.execute(stmnt)
         result = cursor.fetchall()
-        if settings['schema'] not in map(lambda x: x[0], result):
-            raise ValidationError(_("The schema %s defined in settings does not exists"))
+        if settings["schema"] not in map(lambda x: x[0], result):
+            raise ValidationError(
+                _("The schema %s defined in settings does not exists")
+            )
 
         last_update = fields.datetime.now()
 
         ########## Syncronize Products
-        self.synchronize_products(cursor, settings['schema'], last_update, reference=reference)
+        self.synchronize_products(
+            cursor, settings["schema"], last_update, reference=reference
+        )
 
         ########## Syncronize ATP
         # self.synchronize_atp(cursor, settings['schema'], last_update, reference=reference)
@@ -48,11 +62,12 @@ class LightingPortalConnectorSync(models.TransientModel):
         cursor.close()
         conn.close()
 
-        if not reference: _logger.info('End syncronization')
+        if not reference:
+            _logger.info("End syncronization")
 
         return {
-            'type': 'ir.actions.client',
-            'tag': 'reload',
+            "type": "ir.actions.client",
+            "tag": "reload",
         }
 
     @api.model
@@ -65,9 +80,9 @@ class LightingPortalConnectorSync(models.TransientModel):
                         WHERE pw."ItemCode" = p."ItemCode" AND
                               p."ItemType" = 'I' AND
                               pw."WhsCode" = '00'
-                    ), product_capacity AS ( 
+                    ), product_capacity AS (
                         select lml."Father",
-                               min(MAP(ps."InvntItem", 'Y', 
+                               min(MAP(ps."InvntItem", 'Y',
                                        round(ps."Available"/lml."Quantity", 0, ROUND_DOWN))) AS "Capacity"
                         from %(schema)s.ITT1 lml, product_stock ps
                         WHERE lml."Code" = ps."ItemCode"
@@ -76,7 +91,7 @@ class LightingPortalConnectorSync(models.TransientModel):
                         SELECT ps."ItemCode", ps."Available", 0 AS "IsKit"
                         FROM product_stock ps
                         UNION ALL
-                        SELECT pc."Father" AS "ItemCode", 
+                        SELECT pc."Father" AS "ItemCode",
                                (CASE WHEN pc."Capacity"<0 THEN 0 ELSE pc."Capacity" END)  AS "Available",
                                1 AS "IsKit"
                         FROM product_capacity pc
@@ -88,24 +103,30 @@ class LightingPortalConnectorSync(models.TransientModel):
                     WHERE pm."ItemCode" = p."ItemCode" and
                           (:reference is null OR pm."ItemCode" = :reference)
                     GROUP BY pm."ItemCode", p."CodeBars"
-                """ % dict(schema=schema)
+                """ % dict(
+            schema=schema
+        )
 
-        cursor.execute(stmnt, {'reference': reference})
+        cursor.execute(stmnt, {"reference": reference})
         header = [x[0] for x in cursor.description]
         for row in cursor:
             result0_d = dict(zip(header, row))
-            result0_d['qty_available'] = int(result0_d['qty_available'])
-            if result0_d['qty_available'] >= 99:
-                result0_d['qty_available'] = 99
-            elif result0_d['qty_available'] < 0:
-                result0_d['qty_available'] = 0
+            result0_d["qty_available"] = int(result0_d["qty_available"])
+            if result0_d["qty_available"] >= 99:
+                result0_d["qty_available"] = 99
+            elif result0_d["qty_available"] < 0:
+                result0_d["qty_available"] = 0
 
-            pim_product = self.env['lighting.product'].search([('reference', '=', result0_d['reference'])])
+            pim_product = self.env["lighting.product"].search(
+                [("reference", "=", result0_d["reference"])]
+            )
             if pim_product:
-                result0_d['description'] = pim_product.description
-                result0_d['product_id'] = pim_product.id
+                result0_d["description"] = pim_product.description
+                result0_d["product_id"] = pim_product.id
 
-            portal_product = self.env['lighting.portal.product'].search([('reference', '=', result0_d['reference'])])
+            portal_product = self.env["lighting.portal.product"].search(
+                [("reference", "=", result0_d["reference"])]
+            )
             if portal_product:
                 if not pim_product:
                     portal_product.unlink()
@@ -113,22 +134,25 @@ class LightingPortalConnectorSync(models.TransientModel):
                     result1_d = {}
                     for k0, v0 in result0_d.items():
                         v1 = getattr(portal_product, k0, None)
-                        v1 = v1.id if k0 == 'product_id' else v1
+                        v1 = v1.id if k0 == "product_id" else v1
                         if v1 != v0:
                             result1_d[k0] = v0
 
-                    result1_d['last_update'] = last_update
+                    result1_d["last_update"] = last_update
                     portal_product.write(result1_d)
             else:
                 if pim_product:
-                    result0_d['last_update'] = last_update
-                    self.env['lighting.portal.product'].create(result0_d)
+                    result0_d["last_update"] = last_update
+                    self.env["lighting.portal.product"].create(result0_d)
 
         # clean residual portal products
         if not reference:
-            pim_product_references = self.env['lighting.product'].search([]).mapped("reference")
-            portal_product_orphan_ids = self.env['lighting.portal.product'].search(
-                [('reference', 'not in', pim_product_references)])
+            pim_product_references = (
+                self.env["lighting.product"].search([]).mapped("reference")
+            )
+            portal_product_orphan_ids = self.env["lighting.portal.product"].search(
+                [("reference", "not in", pim_product_references)]
+            )
             portal_product_orphan_ids.unlink()
 
     @api.model
@@ -150,27 +174,31 @@ class LightingPortalConnectorSync(models.TransientModel):
                                FROM %(schema)s.OWOR o
                                WHERE (:reference is null OR o."ItemCode" = :reference) AND
                                      o."Warehouse" = '00' AND
-                                     o."Status" IN ('R', 'P') AND 
+                                     o."Status" IN ('R', 'P') AND
                                      (o."PlannedQty" - (o."CmpltQty" + o."RjctQty")) > 0
                            )
-                           SELECT a."ItemCode" AS "reference", a."ShipDate" as "ship_date", 
+                           SELECT a."ItemCode" AS "reference", a."ShipDate" as "ship_date",
                                   sum(a."OnOrder") AS "qty_ordered"
                            FROM atp_onorder a
                            WHERE DAYS_BETWEEN(CURRENT_DATE, a."ShipDate") <= 7*4
                            GROUP BY a."ItemCode", a."ShipDate"
                            ORDER BY "ItemCode", "ShipDate"
-                        """ % dict(schema=schema)
+                        """ % dict(
+            schema=schema
+        )
 
-        cursor.execute(stmnt, {'reference': reference})
+        cursor.execute(stmnt, {"reference": reference})
         header = [x[0] for x in cursor.description]
         atp_d = {}
         for row in cursor:
             result0_d = dict(zip(header, row))
-            result0_d['qty_ordered'] = int(result0_d['qty_ordered'])
+            result0_d["qty_ordered"] = int(result0_d["qty_ordered"])
 
-            portal_product = self.env['lighting.portal.product'].search([('reference', '=', result0_d['reference'])])
+            portal_product = self.env["lighting.portal.product"].search(
+                [("reference", "=", result0_d["reference"])]
+            )
             if portal_product:
-                values = {x: result0_d[x] for x in ['ship_date', 'qty_ordered']}
+                values = {x: result0_d[x] for x in ["ship_date", "qty_ordered"]}
                 atp_d.setdefault(portal_product, []).append(values)
 
         ## updatem
