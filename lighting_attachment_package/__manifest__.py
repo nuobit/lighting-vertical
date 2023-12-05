2e88eb6bf0c160f985fdce2379f3856b8d4af5f9
KNVx <kniubo@nuobit.com>
2023-12-05 16:09:17 +0100

    [IMP] lighting_attachment_package: pre-commit stuff

--- lighting_attachment_package/__manifest__.py
+++ lighting_attachment_package/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Lighting Attachment Package",
-    'description': "Generate attachment packages",
-    'version': '11.0.1.0.6',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': [
-        'lighting',
-        'lighting_seo',
-        'lighting_reporting',
+    "name": "Lighting Attachment Package",
+    "description": "Generate attachment packages",
+    "version": "11.0.1.0.6",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting",
+        "lighting_seo",
+        "lighting_reporting",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/product_attachment_views.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_attachment_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
