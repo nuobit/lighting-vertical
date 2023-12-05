d7a30f483550975eafc02f6e0f20c5aecc702e0d
KNVx <kniubo@nuobit.com>
2023-12-05 16:13:01 +0100

    [IMP] lighting_export: pre-commit stuff

--- lighting_export/models/ir_model.py
+++ lighting_export/models/ir_model.py
@@ -2,15 +2,15 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class IrModelFields(models.Model):
-    _inherit = 'ir.model.fields'
+    _inherit = "ir.model.fields"
 
     @api.multi
     def name_get(self):
         res = []
         for field in self:
-            res.append((field.id, '%s' % (field.field_description, )))
+            res.append((field.id, "%s" % (field.field_description,)))
         return res
