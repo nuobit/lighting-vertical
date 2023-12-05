06623273952e2a76c6e02f883d6c79b827b41319
KNVx <kniubo@nuobit.com>
2023-12-05 16:07:18 +0100

    [IMP] lighting_advanced_search : pre-commit stuff

--- lighting_advanced_search/__manifest__.py
+++ lighting_advanced_search/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Lighting Advanced Search",
-    'description': "Lighting advanced product search",
-    'version': '11.0.0.2.6',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': ['lighting'],
-    'data': [
-        'wizard/product_advanced_search_views.xml',
-        ],
-    'installable': True,
+    "name": "Lighting Advanced Search",
+    "description": "Lighting advanced product search",
+    "version": "11.0.0.2.6",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": ["lighting"],
+    "data": [
+        "wizard/product_advanced_search_views.xml",
+    ],
+    "installable": True,
 }
