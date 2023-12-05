2f8ed055c0787c6398ee50a0ad273284a8c2a7d9
KNVx <kniubo@nuobit.com>
2023-12-05 16:11:52 +0100

    [IMP] lighting_etim: pre-commit stuff

--- lighting_etim/models/etim_class_feature.py
+++ lighting_etim/models/etim_class_feature.py
@@ -2,35 +2,47 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
+from odoo import fields, models
 
 
 class LightingETIMClassFeature(models.Model):
-    _name = 'lighting.etim.class.feature'
-    _order = 'sequence'
+    _name = "lighting.etim.class.feature"
+    _order = "sequence"
 
     sequence = fields.Integer("Order", required=True, default=1)
 
     change_code = fields.Char("Change code", required=True)
 
-    feature_id = fields.Many2one(comodel_name='lighting.etim.feature', ondelete='restrict', string='Feature')
-    unit_id = fields.Many2one(comodel_name='lighting.etim.unit', ondelete='restrict', string='Unit')
+    feature_id = fields.Many2one(
+        comodel_name="lighting.etim.feature", ondelete="restrict", string="Feature"
+    )
+    unit_id = fields.Many2one(
+        comodel_name="lighting.etim.unit", ondelete="restrict", string="Unit"
+    )
 
-    value_ids = fields.One2many(comodel_name='lighting.etim.class.feature.value',
-                                inverse_name='feature_id', string='Values')
+    value_ids = fields.One2many(
+        comodel_name="lighting.etim.class.feature.value",
+        inverse_name="feature_id",
+        string="Values",
+    )
 
-    class_id = fields.Many2one(comodel_name='lighting.etim.class', ondelete='cascade', string='Class')
+    class_id = fields.Many2one(
+        comodel_name="lighting.etim.class", ondelete="cascade", string="Class"
+    )
 
 
 class LightingETIMClassFeatureValue(models.Model):
-    _name = 'lighting.etim.class.feature.value'
-    _order = 'sequence'
+    _name = "lighting.etim.class.feature.value"
+    _order = "sequence"
 
     sequence = fields.Integer("Order", required=True, default=1)
 
     change_code = fields.Char("Change code", required=True)
 
-    value_id = fields.Many2one(comodel_name='lighting.etim.value', ondelete='restrict', string='Value')
+    value_id = fields.Many2one(
+        comodel_name="lighting.etim.value", ondelete="restrict", string="Value"
+    )
 
-    feature_id = fields.Many2one(comodel_name='lighting.etim.class.feature', ondelete='cascade', string='Feature')
+    feature_id = fields.Many2one(
+        comodel_name="lighting.etim.class.feature", ondelete="cascade", string="Feature"
+    )
