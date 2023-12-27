c67109f12f862aa7c5da3f1399db24b58af25ee4
KNVx <kniubo@nuobit.com>
2023-12-27 12:43:46 +0100

    [IMP] lighting_reporting_attachment: pre-commit stuff

--- lighting_reporting_attachment/__manifest__.py
+++ lighting_reporting_attachment/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Lighting Reporting Attachment",
-    'description': """* Enables the option to use a selected attachments as a product datasheet""",
-    'version': '11.0.1.0.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': [
-        'lighting_reporting',
+    "name": "Lighting Reporting Attachment",
+    "description": """* Enables the option to use a selected attachments as a product datasheet""",
+    "version": "11.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting_reporting",
     ],
-    'data': [
-        'views/product_attachment_views.xml',
+    "data": [
+        "views/product_attachment_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- lighting_reporting_attachment/models/product_attachment.py
+++ lighting_reporting_attachment/models/product_attachment.py
@@ -2,10 +2,12 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class LightingAttachment(models.Model):
-    _inherit = 'lighting.attachment'
+    _inherit = "lighting.attachment"
 
-    use_as_product_datasheet = fields.Boolean(string="Use as a product datasheet", default=False)
+    use_as_product_datasheet = fields.Boolean(
+        string="Use as a product datasheet", default=False
+    )
