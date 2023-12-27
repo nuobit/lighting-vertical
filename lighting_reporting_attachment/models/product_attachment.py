5d6247f910509cb98f7cd30afd99d4a327322fce
KNVx <kniubo@nuobit.com>
2023-12-27 12:49:29 +0100

    [MIG] lighting_reporting_attachment: Migration to 16.0

--- lighting_reporting_attachment/models/product_attachment.py
+++ lighting_reporting_attachment/models/product_attachment.py
@@ -1,5 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import fields, models
@@ -9,5 +8,5 @@ class LightingAttachment(models.Model):
     _inherit = "lighting.attachment"
 
     use_as_product_datasheet = fields.Boolean(
-        string="Use as a product datasheet", default=False
+        default=False,
     )
