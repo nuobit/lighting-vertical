25fedba0fc18e7abd592abbb9e8694096ba253f3
KNVx <kniubo@nuobit.com>
2024-01-11 10:26:45 +0100

    [IMP] lighting_topbar: pre-commit stuff

--- lighting_topbar/__manifest__.py
+++ lighting_topbar/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Lighting Top bar",
-    'description': "Remove backoffice root menus and systray",
-    'version': '11.0.0.2.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': ['base', 'web'],
-    'data': [
-        'security/topbar_security.xml',
-        'views/web_templates.xml',
-        ],
-    'installable': True,
+    "name": "Lighting Top bar",
+    "description": "Remove backoffice root menus and systray",
+    "version": "11.0.0.2.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": ["base", "web"],
+    "data": [
+        "security/topbar_security.xml",
+        "views/web_templates.xml",
+    ],
+    "installable": True,
 }
