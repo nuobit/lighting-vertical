771942c6d339374c6af60b604951910760c538f4
KNVx <kniubo@nuobit.com>
2024-01-15 08:34:40 +0100

    [MIG] lighting_reporting_seo: Migration to 16.0

---
+++ lighting_reporting_seo/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Lighting Reporting SEO",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "license": "AGPL-3",
+    "category": "Lighting",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting_reporting",
+        "lighting_seo",
+    ],
+    "data": [
+        "views/report_product.xml",
+    ],
+    "auto_install": True,
+}
