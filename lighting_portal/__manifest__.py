6d20263d6dd26f02c4acd8ce64cbd8ec81cb9424
KNVx <kniubo@nuobit.com>
2024-01-17 17:21:09 +0100

    [MIG] lighting_portal: pre-commit stuff

--- lighting_portal/__manifest__.py
+++ lighting_portal/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Lighting Portal",
-    'description': "Lighting Portal",
-    'version': '11.0.0.6.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': [
-        'lighting',
-        'report_xml',
+    "name": "Lighting Portal",
+    "description": "Lighting Portal",
+    "version": "11.0.0.6.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting",
+        "report_xml",
     ],
-    'data': [
-        'security/portal_security.xml',
-        'security/ir.model.access.csv',
-        'report/report.xml',
-        'views/portal_views.xml',
-        'views/report_product_xml.xml',
+    "data": [
+        "security/portal_security.xml",
+        "security/ir.model.access.csv",
+        "report/report.xml",
+        "views/portal_views.xml",
+        "views/report_product_xml.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- lighting_portal/controllers/main.py
+++ lighting_portal/controllers/main.py
@@ -1,10 +1,9 @@
 import base64
-
-from odoo import http
+import logging
 
 import werkzeug.exceptions
 
-import logging
+from odoo import http
 
 _logger = logging.getLogger(__name__)
 
@@ -13,33 +12,48 @@ class ProductDatasheetController(http.Controller):
     def get_products_xml(self, lang=None):
         # lang check
         if not lang:
-            lang = 'es_ES'
+            lang = "es_ES"
 
-        lang_id = http.request.env['res.lang'].search([
-            ('active', '=', True),
-            ('code', '=', lang),
-        ])
+        lang_id = http.request.env["res.lang"].search(
+            [
+                ("active", "=", True),
+                ("code", "=", lang),
+            ]
+        )
         if len(lang_id) == 0:
-            return werkzeug.exceptions.NotFound('The %s language does not exist' % lang)
+            return werkzeug.exceptions.NotFound("The %s language does not exist" % lang)
         elif len(lang_id) > 1:
-            return werkzeug.exceptions.InternalServerError('More than one language found with code %s' % lang)
+            return werkzeug.exceptions.InternalServerError(
+                "More than one language found with code %s" % lang
+            )
 
         # generate report
-        report = http.request.env.ref('lighting_portal.action_report_product_xml')
-        product_ids = http.request.env['lighting.portal.product'].search([]) \
-            .sorted(lambda x: x.reference).mapped('id')
-        xml_products = report.with_context(lang=lang).render_qweb_xml(product_ids, {})[0]
+        report = http.request.env.ref("lighting_portal.action_report_product_xml")
+        product_ids = (
+            http.request.env["lighting.portal.product"]
+            .search([])
+            .sorted(lambda x: x.reference)
+            .mapped("id")
+        )
+        xml_products = report.with_context(lang=lang).render_qweb_xml(product_ids, {})[
+            0
+        ]
 
         # return report
         xmlhttpheaders = [
-            ('Content-Type', 'application/xml'),
-            ('Content-Length', len(xml_products)),
+            ("Content-Type", "application/xml"),
+            ("Content-Length", len(xml_products)),
         ]
         return http.request.make_response(xml_products, headers=xmlhttpheaders)
 
-    @http.route(['/services/products',
-                 '/services/<string:lang>/products',
-                 ], type='http', auth="none")
+    @http.route(
+        [
+            "/services/products",
+            "/services/<string:lang>/products",
+        ],
+        type="http",
+        auth="none",
+    )
     def download_products_xml(self, lang=None):
         # Check for Odoo session authentication
         if http.request.session.uid:
@@ -48,21 +62,27 @@ class ProductDatasheetController(http.Controller):
 
         # Check for HTTP Basic Authentication
         headers = http.request.httprequest.headers
-        basic_auth_header = headers.get('Authorization')
+        basic_auth_header = headers.get("Authorization")
         if basic_auth_header:
             try:
                 auth_type, encoded_credentials = basic_auth_header.split(None, 1)
-                if auth_type.lower() != 'basic':
-                    raise werkzeug.exceptions.Unauthorized('Unsupported authentication method')
-                username, password = base64.b64decode(encoded_credentials).decode().split(':', 1)
+                if auth_type.lower() != "basic":
+                    raise werkzeug.exceptions.Unauthorized(
+                        "Unsupported authentication method"
+                    )
+                username, password = (
+                    base64.b64decode(encoded_credentials).decode().split(":", 1)
+                )
             except Exception as e:
                 msg = "Error processing authentication"
                 _logger.error(msg + f": {e}")
                 raise werkzeug.exceptions.BadRequest(msg)
-            uid = http.request.session.authenticate(http.request.session.db, username, password)
+            uid = http.request.session.authenticate(
+                http.request.session.db, username, password
+            )
             if not uid:
-                raise werkzeug.exceptions.Unauthorized('Invalid credentials')
+                raise werkzeug.exceptions.Unauthorized("Invalid credentials")
             return self.get_products_xml(lang)
 
         # No authentication
-        raise werkzeug.exceptions.Unauthorized('Invalid authentication')
+        raise werkzeug.exceptions.Unauthorized("Invalid authentication")
