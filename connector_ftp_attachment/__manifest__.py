dc2d87dc15d9ab1572e3ca87f2a8b47f0c01e7f3
KNVx <kniubo@nuobit.com>
2024-01-22 12:04:27 +0100

    [IMP] connector_ftp_attachment: pre-commit stuff

--- connector_ftp_attachment/__manifest__.py
+++ connector_ftp_attachment/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Connector FTP attachment",
-    'description': "Connector to upload attachments to FTP",
-    'version': '11.0.1.0.2',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Connector',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'lighting_attachment_package',
-        'connector',
+    "name": "Connector FTP attachment",
+    "description": "Connector to upload attachments to FTP",
+    "version": "11.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting_attachment_package",
+        "connector",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/ir_cron.xml',
-        'views/backend_views.xml',
-        'views/menus.xml',
-        'views/product_attachment_views.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+        "views/backend_views.xml",
+        "views/menus.xml",
+        "views/product_attachment_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- connector_ftp_attachment/models/product_attachment_package.py
+++ connector_ftp_attachment/models/product_attachment_package.py
@@ -2,14 +2,13 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.queue_job.job import job
+from odoo import fields, models
 
-from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError, UserError
+from odoo.addons.queue_job.job import job
 
 
 class LightingAttachmentPackage(models.Model):
-    _inherit = 'lighting.attachment.package'
+    _inherit = "lighting.attachment.package"
 
     auto = fields.Boolean(
         string="Auto",
@@ -21,18 +20,18 @@ class LightingAttachmentPackage(models.Model):
         readonly=True,
     )
 
-    @job(default_channel='root.ftpattach')
+    @job(default_channel="root.ftpattach")
     def upload(self, backend, generate=False):
         self.ensure_one()
         if generate:
             self.generate_file_button()
         ftp = backend.get_ftp_connection()
-        full_path = self.env['ir.attachment']._full_path(self.attachment_id.store_fname)
+        full_path = self.env["ir.attachment"]._full_path(self.attachment_id.store_fname)
         self.last_upload = fields.datetime.now()
-        with open(full_path, 'rb') as f:
-            ftp.storbinary('STOR %s' % self.datas_fname, f)
+        with open(full_path, "rb") as f:
+            ftp.storbinary("STOR %s" % self.datas_fname, f)
         ftp.quit()
 
     def upload_button(self):
-        backend = self.env['ftp.attachment.backend'].get_default()
+        backend = self.env["ftp.attachment.backend"].get_default()
         self.upload(backend)
