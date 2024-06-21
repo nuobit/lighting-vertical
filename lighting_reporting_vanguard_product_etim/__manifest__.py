33705508d6bd98255c68d5801e8b7ae2c502d7c9
FrankC013 <fcespedes@nuobit.com>
2024-06-21 14:04:13 +0200

    [ADD] lighting_reporting_vanguard_product_etim

---
+++ lighting_reporting_vanguard_product_etim/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Lighting Reporting Vanguard Product ETIM",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "license": "AGPL-3",
+    "category": "Lighting",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting_reporting_vanguard_product",
+        "lighting_etim",
+    ],
+    "data": [
+        "views/report_vanguard.xml",
+    ],
+    "auto_install": True,
+}
---
+++ lighting_reporting_vanguard_product_etim/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `NuoBiT <https://www.nuobit.com>`__:
+
+  * Kilian Niubo <kniubo@nuobit.com>
+  * Frank Cespedes <fcespedes@nuobit.com>
+  * Eric Antones <eantones@nuobit.com>
