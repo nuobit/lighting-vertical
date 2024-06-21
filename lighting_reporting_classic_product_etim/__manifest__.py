3165a01c56a883afcdb374929e3bbae3dee4373b
FrankC013 <fcespedes@nuobit.com>
2024-06-21 14:23:24 +0200

    [ADD] lighting_reporting_classic_product_etim

---
+++ lighting_reporting_classic_product_etim/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Lighting Reporting ETIM",
+    "summary": "Includes lighting seo on lighting reporting classic.",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "license": "AGPL-3",
+    "category": "Lighting",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": [
+        "lighting_reporting_classic_product",
+        "lighting_etim",
+    ],
+    "data": [
+        "views/report_classic.xml",
+    ],
+    "auto_install": True,
+}
