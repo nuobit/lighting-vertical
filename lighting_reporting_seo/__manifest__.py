3fd9517ac0eda8eb77c021e8c4fc64fe635ca0bb
FrankC013 <fcespedes@nuobit.com>
2024-07-10 10:13:39 +0200

    [REM] lighting_reporting_seo: mark module as deprecated

--- lighting_reporting_seo/__manifest__.py
+++ lighting_reporting_seo/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    "name": "Lighting Reporting SEO",
+    "name": "(DEPRECATED) Lighting Reporting SEO",
     "version": "16.0.1.0.0",
     "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
