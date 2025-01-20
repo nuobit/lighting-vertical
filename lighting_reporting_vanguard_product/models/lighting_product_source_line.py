38b9510b0c6b7d7697f3c37ea227996e9a0773be
Eric Antones <eantones@nuobit.com>
2025-01-20 19:20:20 +0100

    [IMP] lighting_reporting_vanguard_product: add source count in front of nominal flux, total flux and wattage

--- lighting_reporting_vanguard_product/models/lighting_product_source_line.py
+++ lighting_reporting_vanguard_product/models/lighting_product_source_line.py
@@ -8,6 +8,14 @@ from odoo import models
 class LightingProductSourceLine(models.Model):
     _inherit = "lighting.product.source.line"
 
+    def _prepend_source_num(self, value):
+        self.ensure_one()
+        values_l = []
+        if self.source_id.num > 1:
+            values_l.append("(%ix)" % self.source_id.num)
+        values_l.append(value)
+        return "".join(values_l)
+
     def _get_color_temperature_flux_values(self, flux_attr):
         self.ensure_one()
         found = False
@@ -25,11 +33,26 @@ class LightingProductSourceLine(models.Model):
 
     def get_nominal_flux_display(self):
         self.ensure_one()
-        return self._get_color_temperature_flux_values("nominal_flux")
+        return self._prepend_source_num(
+            self._get_color_temperature_flux_values("nominal_flux")
+        )
 
     def get_total_flux_display(self):
         self.ensure_one()
-        return self._get_color_temperature_flux_values("total_flux")
+        return self._prepend_source_num(
+            self._get_color_temperature_flux_values("total_flux")
+        )
+
+    def get_total_wattage_display(self):
+        self.ensure_one()
+        wattage_magnitude_map = dict(
+            self.fields_get("wattage_magnitude")["wattage_magnitude"]["selection"]
+        )
+        value = [
+            self.get_format_lang_decimal(self.wattage),
+            wattage_magnitude_map[self.wattage_magnitude],
+        ]
+        return self._prepend_source_num(" ".join(value))
 
     def get_color_consistency_display(self):
         self.ensure_one()
