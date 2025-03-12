25a54df0413e0c88c28a7662ad79848a57d0f20b
FrankC013 <fcespedes@nuobit.com>
2025-03-12 18:16:42 +0100

    [ADD] lighting_export_bmecat_seo

---
+++ lighting_export_bmecat_seo/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lighting Export BMEcat SEO",
+    "summary": "This module adds SEO fields to the BMEcat catalog.",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "license": "AGPL-3",
+    "category": "Lighting",
+    "website": "https://github.com/NuoBiT/lighting-vertical",
+    "depends": ["lighting_export_bmecat", "lighting_seo"],
+    "data": [
+        "views/report_bmecat.xml",
+    ],
+}
