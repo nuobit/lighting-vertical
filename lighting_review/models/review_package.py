f3b0af4b75c554a82ae39ed524e2837545334e2d
bijaya <kumalbijay05@gmail.com>
2025-05-21 14:53:18 +0200

    [FIX] lighting_review: Fix warnings

--- lighting_review/models/review_package.py
+++ lighting_review/models/review_package.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import datetime
@@ -8,6 +9,7 @@ from odoo import _, api, fields, models
 
 class LightingProductReview(models.Model):
     _name = "lighting.review.package"
+    _description = "Lighting Product Review Package"
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
     _order = "name"
