b0d6b0f4ea2a2389353ec3d7d6a5b648279f2bc6
bijaya <kumalbijay05@gmail.com>
2025-05-21 14:50:21 +0200

    [FIX] lighting_etim: Fix warnings

--- lighting_etim/models/etim_class_synonim.py
+++ lighting_etim/models/etim_class_synonim.py
@@ -1,5 +1,6 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import fields, models
@@ -7,6 +8,7 @@ from odoo import fields, models
 
 class LightingETIMClassSynonim(models.Model):
     _name = "lighting.etim.class.synonim"
+    _description = "ETIM Class Synonim"
 
     name = fields.Char(
         string="Synonim",
--- lighting_etim/models/etim_value.py
+++ lighting_etim/models/etim_value.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
@@ -6,6 +7,7 @@ from odoo import api, fields, models
 
 class LightingETIMValue(models.Model):
     _name = "lighting.etim.value"
+    _description = "ETIM Value"
     _order = "code"
 
     code = fields.Char(
