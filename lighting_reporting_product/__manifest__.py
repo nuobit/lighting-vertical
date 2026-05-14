126c9bc64835eeace8c70a62856ca529708c59f7
Eric Antones <eantones@nuobit.com>
2026-05-14 16:10:32 +0200

    [REF] lighting_reporting_product: exclude components from related-products filter
    Mirror of the same fix in lighting_reporting: the related-products
    computation on the product datasheet now treats a root category as
    exclusion-worthy when either is_accessory or is_component is set.
    
    Manifest bumped 16.0.1.0.0 -> 16.0.1.2.0.

--- lighting_reporting_product/__manifest__.py
+++ lighting_reporting_product/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Lighting Reporting Product",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.2.0",
     "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Lighting",
